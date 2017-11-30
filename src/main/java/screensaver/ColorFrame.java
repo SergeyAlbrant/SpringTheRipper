39a8ef8d360e9577c90af9fa1d8735dc00cafef4
SergeyAlbrant <albrants95@mail.ru>
2017-12-01 00:49:59 +0300

    initial colorFrame

---
+++ src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,24 @@
+package screensaver;
+
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+@Component
+public class ColorFrame extends JFrame {
+    private Color color;
+
+    public ColorFrame() throws HeadlessException {
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace(){
+        Random random = new Random();
+        setLocation(random.nextInt(1200),random.nextInt(700));
+        getContentPane().setBackground(color);
+        repaint();
+    }
+}
